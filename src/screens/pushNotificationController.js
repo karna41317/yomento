4a952d24cd01ac62245a9b84d573d55ab57f086d
Karan <karan.antham@gmail.com>
2017-12-16 23:57:31 +0100

    pusher

--- src/screens/pushNotificationController.js
+++ src/screens/pushNotificationController.js
@@ -1,27 +1,35 @@
-/**
- * Created by Karan on 2017-12-09.
- */
 import React, { Component } from 'react'
-import {AppState} from 'react-native'
+import { AppState } from 'react-native'
 import PushNotification from 'react-native-push-notification'
+import Pusher from 'pusher-js/react-native'
 
 export default class PushController extends Component {
   componentDidMount () {
     PushNotification.configure({
       onNotification: function (notification) {
       },
-      onRegister: function(token) {
+      onRegister: function (token) {
       },
       permissions: {
         alert: true,
         badge: true,
-        sound: true
+        sound: true,
       },
       popInitialNotification: true,
       requestPermissions: true,
     })
 
+    Pusher.logToConsole = true
 
+    const pusher = new Pusher('a5b3fa8839d4a6a94840', {
+      cluster: 'eu',
+      encrypted: true,
+    })
+
+    const channel = pusher.subscribe('my-channel')
+    channel.bind('my-event', function (data) {
+      console.log('pringingdevicetoken', data)
+    })
 
   }
 
